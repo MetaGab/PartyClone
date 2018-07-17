339982a52ada0315ae45da3ede42d4508771fcf0
Gabo B <gabo_banda@outlook.com>
2018-07-16 21:01:00 -0700

    Start of Branch, advance on Hexagon Heat

--- Assets/HexagonHeat/GameController.cs
+++ Assets/HexagonHeat/GameController.cs
@@ -8,7 +8,10 @@ public class GameController : MonoBehaviour {
 	[SerializeField]
 	private GameObject[] floors = new GameObject[7];
 
-	bool dropped = false;
+	private int dropState = 0;
+	private int random;
+	private float speed = 2f;
+	private float timeTotal = 0; 
 
 	void Start () {
 		
@@ -17,9 +20,25 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (!dropped){
-			int random = Random.Range(0,6);
+		timeTotal += Time.deltaTime;
+
+		if (dropState == 0){
+			random = Random.Range(0,6);
+			dropState = 1;
 		}
-	
+		else if(dropState == 1){
+			floors[random].transform.position = new Vector3(floors[random].transform.position.x, floors[random].transform.position.y-speed*Time.deltaTime, floors[random].transform.position.z);
+			if(floors[random].transform.position.y < -4){
+				dropState = 2;
+			}
+		}
+		else if(dropState == 2){
+			floors[random].transform.position = new Vector3(floors[random].transform.position.x, floors[random].transform.position.y+speed*Time.deltaTime, floors[random].transform.position.z);
+			if(floors[random].transform.position.y > 0){
+				floors[random].transform.position = new Vector3(floors[random].transform.position.x, 0, floors[random].transform.position.z);
+				dropState = 0;
+				speed += .5f;
+			}
+		}
 	}
 }
