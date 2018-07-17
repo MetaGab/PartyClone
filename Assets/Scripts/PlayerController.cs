bfb14e1bee427575d8a198b180845333c79eed3a
KevinCaballero1 <kevin.199907@hotmail.com>
2018-07-16 20:24:50 -0700

    TokenGame

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]  float speed = 256f;
+    public Rigidbody rb;
+  
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce((-10f * speed), 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce((10f * speed), 0f, 0f); 
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(0f, 0f, (-10f * speed));
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(0f, 0f, (10f * speed)); 
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnToken.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnToken : MonoBehaviour {
+    public GameObject token;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Token")
+        {
+            Destroy(other.gameObject);
+            Vector3 spawnPosition = new Vector3(0f, 10f, 0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            GameObject rs = Instantiate(token, spawnPosition, spawnRotation);
+        }  
+    }
+}
