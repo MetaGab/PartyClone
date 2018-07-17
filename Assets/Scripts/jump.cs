5d7623e87abe856a80fb98941f1a59c9d1191404
Jorgect99 <jorgect99@gmail.com>
2018-07-16 20:38:59 -0500

    toadinthebox

---
+++ Assets/Scripts/jump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+    private bool grounded = true;
+    private Rigidbody rb;
+    void Start () {
+        GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.Space) && grounded==true){
+            rb.AddForce(0, 500f, 0);
+            grounded = false;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    [SerializeField] float speed = 3f;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        //print("hola soy un cubo");
+        transform.Rotate(new Vector3(-30f, 0f, 0f) * Time.deltaTime * speed);
+	}
+}
